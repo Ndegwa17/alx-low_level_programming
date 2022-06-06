a8c5b1e67abbc2522538c3f0902f07664cf7a973
Calvin Ndegwa <ndegwacalvin2@gmail.com>
2022-06-06 09:23:28 -0700

    Task_3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - All codes
+ * Description: A program that prints alphabets in lowercase followed by a line
+ *
+ * Return: 0
+ *
+ */
+int main (void)
+{
+	char alphabets;
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	putchar('\n');
+	return (0);
+}
