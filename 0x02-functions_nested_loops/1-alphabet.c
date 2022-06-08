20afa481e2d1d567b40697981e94b4cf4b95bb35
Calvin Ndegwa <ndegwacalvin2@gmail.com>
2022-06-08 09:12:59 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
